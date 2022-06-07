8fef60a0d0995ad22fb1a7edca444040eea1dc1c
Maycon Oliveira <rick.maycon.98@gmail.com>
2022-06-07 20:11:45 -0300

    feat: implementing LeilaoDaoMock class

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -9,6 +9,42 @@ use Alura\Leilao\Dao\Leilao as LeilaoDao;
 use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 
+class LeilaoDaoMock extends LeilaoDao {
+
+    private $leiloes = [];
+
+    public function salva(Leilao $leilao): void
+    {
+        $this->leiloes[] = $leilao;
+    }
+
+    /**
+     * @return array|Leilao[]
+     */
+    public function recuperarNaoFinalizados(): array
+    {
+        return array_filter($this->leiloes, function (Leilao $leilao) {
+           return !$leilao->estaFinalizado();
+        });
+    }
+
+    public function atualiza(Leilao $leilao): void
+    {
+
+    }
+
+    /**
+     * @return array|Leilao[]
+     */
+    public function recuperarFinalizados(): array
+    {
+        return array_filter($this->leiloes, function (Leilao $leilao) {
+            return $leilao->estaFinalizado();
+        });
+    }
+
+}
+
 class EncerradorTest extends TestCase
 {
 
@@ -17,11 +53,11 @@ class EncerradorTest extends TestCase
         $fiatMobi = new Leilao('Fiat Mobi 2021', new \DateTimeImmutable('8 days ago'));
         $fiatArgo = new Leilao('Fiat Argo 2021', new \DateTimeImmutable('10 days ago'));
 
-        $leilaoDao = new LeilaoDao();
+        $leilaoDao = new LeilaoDaoMock();
         $leilaoDao->salva($fiatMobi);
         $leilaoDao->salva($fiatArgo);
 
-        $encerrador = new Encerrador();
+        $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
 
         $finalizados = $leilaoDao->recuperarFinalizados();
@@ -29,6 +65,5 @@ class EncerradorTest extends TestCase
         self::assertCount(2, $finalizados);
         self::assertEquals('Fiat Mobi 2021', $finalizados[0]->recuperarDescricao());
         self::assertEquals('Fiat Argo 2021', $finalizados[1]->recuperarDescricao());
-
     }
 }
