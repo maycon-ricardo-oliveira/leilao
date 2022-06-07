119e45db92eb1a2a55c2d068920efb7a56e0654d
Maycon Oliveira <rick.maycon.98@gmail.com>
2022-06-07 12:08:09 -0300

    tests: ensure that an auction receives bids

--- tests/Model/LeilaoTest.php
+++ tests/Model/LeilaoTest.php
@@ -1,9 +1,47 @@
 <?php
 namespace Leilao\Tests\Model;
 
+use Leilao\Model\Lance;
+use Leilao\Model\Leilao;
+use Leilao\Model\Usuario;
 use PHPUnit\Framework\TestCase;
 
 class LeilaoTest extends TestCase
 {
+    /**
+     * @param int $quantidadeLances
+     * @param Leilao $leilao
+     * @param array $valores
+     * @return void
+     * @dataProvider geraLances
+     */
+    public function testLeilaoDeveReceberLances(int $quantidadeLances, Leilao $leilao, array $valores)
+    {
+
+        static::assertCount($quantidadeLances, $leilao->getLances());
+
+        foreach ($valores as $key => $valor) {
+            static::assertEquals($valor, $leilao->getLances()[$key]->getValor());
+        }
+
+    }
+
+    public function geraLances()
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilaoComDoisLances = new Leilao('Palio 2003');
+        $leilaoComDoisLances->recebeLance(new Lance($joao, 1000));
+        $leilaoComDoisLances->recebeLance(new Lance($maria, 2000));
+
+        $leilaoComUmLance = new Leilao('Fusca 1972');
+        $leilaoComUmLance->recebeLance(new Lance($maria, 5000));
+
+        return [
+            '2-lances' => [2, $leilaoComDoisLances, [1000, 2000]],
+            '1-lance' => [1, $leilaoComUmLance, [5000]]
+        ];
+    }
 
 }
