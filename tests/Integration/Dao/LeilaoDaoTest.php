9557b4c7b4f66a462220948da209b1b5e68a8548
Maycon Oliveira <rick.maycon.98@gmail.com>
2022-06-13 19:40:56 -0300

    feat: using tearDown function

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -2,7 +2,40 @@
 
 namespace Alura\Leilao\Tests\Integration\Dao;
 
-class LeilaoDaoTest
+use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use Alura\Leilao\Infra\ConnectionCreator;
+use Alura\Leilao\Model\Leilao;
+use PHPUnit\Framework\TestCase;
+
+
+class LeilaoDaoTest extends TestCase
 {
 
+    private $pdo;
+
+    protected function setUp(): void
+    {
+        $this->pdo = ConnectionCreator::getConnection();
+    }
+
+    public function testInsercaoEBuscaDevemFuncionar()
+    {
+
+        $leilao = new Leilao('Fusca 1947');
+        $leilaoDao = new LeilaoDao($this->pdo);
+
+        $leilaoDao->salva($leilao);
+
+        $leiloes = $leilaoDao->recuperarNaoFinalizados();
+
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+
+        self::assertSame('Fusca 1947', $leiloes[0]->recuperarDescricao());
+    }
+
+    protected function tearDown(): void
+    {
+        $this->pdo->exec('DELETE FROM leiloes');
+    }
 }
